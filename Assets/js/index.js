c6404a8476f521d8e56bda1a8248f259d4e1e776
tubo2154 <viet.nt.2409@aptechlearning.edu.vn>
2023-11-29 23:20:31 +0700

    handleToggleReport

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,8 +1,11 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-const iconManager = $('.function-icon_arrow')
-const listManagerNav = $('.nav_bar-function_child')
+const iconManager = $('.function-icon_arrow_Manager')
+const listManagerNav = $('.nav_bar-function_child_Manager')
+const iconReport = $('.function-icon_arrow_Report')
+const listReportNav = $('.nav_bar-function_child_Report')
+
 
 
 const navBar = {
@@ -11,11 +14,18 @@ const navBar = {
       listManagerNav.classList.toggle('none')
     }
   },
+  handleToggleReport: function(){
+    iconReport.onclick = function(){
+      listReportNav.classList.toggle('none')
+    }
+  },
+
 
   
 
   start: function(){
     this.handleToggleManager()
+    this.handleToggleReport()
   },
 }
 // khi phương thức .start được gọi -> tất cả các phương thức trong navBar sẽ được thực thi
