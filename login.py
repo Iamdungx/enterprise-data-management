6f3d6994bc73fec4e294437a3907539909f87f05
ntvd5823 <nguyendung5823@gmail.com>
2024-01-28 01:08:55 +0700

    update login

--- login.py
+++ login.py
@@ -1,44 +1,48 @@
-from flask import Flask, session, render_template, request, redirect, jsonify
-import pyrebase
+from flask import Flask, session, render_template, request, redirect, jsonify, url_for
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-config = {
-    'apiKey': "AIzaSyCiM3nEkc0Eh3rpyOTELTtZnxz8rn2fafc",
-    'authDomain': "enterprise-data-manager.firebaseapp.com",
-    'projectId': "enterprise-data-manager",
-    'storageBucket': "enterprise-data-manager.appspot.com",
-    'messagingSenderId': "798644344070",
-    'appId': "1:798644344070:web:53485cefdb56d98624fa87",
-    'measurementId': "G-4QKL3S5TER",
-    'databaseURL': ''
-}
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'enterprise_data_management'
 
 app.secret_key = 'secret'
 
+mySQL = MySQL(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def authenticate():
+    
     login_failed_message = None  # Initialize the variable
-    if 'user' in session:
-        return login()
+    
+    if request.method == 'GET':
+        return render_template('index.html')
+    
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        try:
-            user = auth.sign_in_with_email_and_password(username, password)
+        
+        cursor = mySQL.connection.cursor()
+        cursor.execute(''' SELECT username, password FROM login WHERE username = %s AND password = %s ''', (username, password))
+        user = cursor.fetchone()
+        cursor.close()
+        
+        if user:
             session['user'] = username
             return render_template('employee-manager.html')
-        except:
+        else:
             login_failed_message = 'Đăng nhập thất bại !'
-    return render_template('index.html', login_failed_message=login_failed_message)
+            return render_template('index.html', login_failed_message=login_failed_message)
 
-def login():
-    return render_template('employee-manager.html')
-
+@app.route('/manager')
+def manager():
+    if 'user' in session:
+        return render_template('employee-manager.html')
+    else:
+        return redirect(url_for('login'))
+    
 # @app.route('/logout')
 # def logout():
 #     # Xóa phiên đăng nhập và chuyển hướng về trang đăng nhập
@@ -52,4 +56,4 @@ def logout():
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run(port=1111, debug=True)
+    app.run(debug=True)
