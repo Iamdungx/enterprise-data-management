cad8ad1fc664e1fcbc098809a6f97f927bc96e26
Minh190903 <149696467+Minh190903@users.noreply.github.com>
2024-04-20 14:14:07 +0700

    payroll-details
    Còn update thêm

--- templates/payroll.php
+++ templates/payroll.php
@@ -1,3 +1,9 @@
+<?php
+    require 'connect_database.php';
+    mysqli_set_charset($connect, 'UTF8');
+    $sql = "SELECT count(*) as tong_so_ngay_cong, COUNT(CASE WHEN a.total < '08:00:00' THEN 1 ELSE NULL END) as so_ngay_thieu_gio, u.*, s.* FROM `user_data` u INNER JOIN `salary_and_bonus` s ON u.id = s.employee_id INNER JOIN `attendance` a ON u.user_id = a.user_id WHERE a.total > '00:00:00' GROUP BY a.user_id;";
+    $result = $connect->query($sql);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,9 +20,44 @@
     <!-- Js -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
+    <style>
+        .link_home {
+            margin-right: 10px;
+            background-color: #9FD7F9;
+            color: black;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            margin: 20px 10px;
+        }
+        .blue-box {
+            padding: 10px; /* Khoảng cách giữa nội dung và viền của ô */
+            border-radius: 5px; /* Bo tròn viền của ô */
+            text-align: center; /* Canh giữa nội dung */
+            margin: 20px 10px;
+        }
+        .blue-box h1 {
+            color: black; /* Màu chữ trắng */
+            margin: 0; /* Xóa khoảng cách lề */
+        }
+        #information-table {
+            border-collapse: collapse;
+            width: 100%;
+            height: auto;
+        }
+        #information-table th, #information-table td {
+            border: 1px solid #dddddd;
+            padding: 8px;
+            text-align: left;
+        }
+        #information-table tr:first-child th {
+            position: sticky; /* Giữ vị trí */
+            top: 0; /* Vị trí cố định ở trên */
+            background-color: #9FD7F9; /* Màu nền của hàng đầu tiên */
+        }
+    </style>
 </head>
 <body>
-    <!-- header -->
     <header class="header">
         <div class="hrm-title">
             <div class="title close">
@@ -83,7 +124,7 @@
                 <div class="nav_bar-function_child">
                     <ul class="nav_bar-function_child_Manager none">
                         <li class="nav_bar-list-item">
-                            <a href="emplpyee_profile.php">Thông tin nhân viên chi tiết</a>
+                            <a href="employee-information.php">Quản lý nhân viên</a>
                         </li>
                         <li class="nav_bar-list-item"><a href="salary.php">Bảng lương</a></li>
                         <li class="nav_bar-list-item"><a href="benefit.php">Bảo hiểm, đãi ngộ</a></li>
@@ -103,9 +144,6 @@
                 </div>
                 <div class="nav_bar-function_child">
                     <ul class="nav_bar-function_child_Report none">
-                        <li class="nav_bar-list-item">Báo cáo theo tuần</li>
-                        <li class="nav_bar-list-item">Danh sách ca</li>
-                        <li class="nav_bar-list-item">Báo cáo theo tháng</li>
                         <li class="nav_bar-list-item"><a href="attendance_report.php">Danh sách chấm công</a></li>
                     </ul>
 
@@ -126,9 +164,7 @@
                         <?php
                             if(isset($_SESSION['role'])){
                                 if($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'manager'){
-                                    echo '<li class="nav_bar-list-item"><a href="form_take_off.php">Đơn xin nghỉ</a></li>
-                                    <li class="nav_bar-list-item"><a href="form_change_shift.php">Đơn xin đổi ca</a></li>
-                                    <li class="nav_bar-list-item"><a href="form_explanation.php">Đơn giải trình</a></li>
+                                    echo '<li class="nav_bar-list-item"><a href="approve_form.php">Duyệt đơn</a></li>
                                     <li class="nav_bar-list-item"><a href="unexcused.php">Nghỉ không phép</a></li>';
                                 }
                             }
@@ -182,8 +218,45 @@
                     }
                     
                 ?>
+
+
             </div>
         </div>
+    <div class="form-edit">
+        <a class="link_home" href='employee-information.php'>Trang chủ</a>
+        <div class="blue-box">
+            <h1>Bảng lương nhân viên</h1>
+        </div>
+    <div class="form-data_manager-table">
+        <table id="information-table">
+            <tr>
+                <th>Họ và tên</th>
+                <th>Lương cơ bản</th>
+                <th>Phụ cấp</th>
+                <th>Ngày công</th>
+                <th>Ngày làm thiếu giờ</th>
+                <th>Tổng lương</th>
+            </tr>
+            <?php
+                while ($row = $result->fetch_assoc()) {
+                    $cong_thuc_te = 30;
+                    $salary = $row['salary'];
+                    $total_salary = $salary * $row['tong_so_ngay_cong'] / $cong_thuc_te - ($row['so_ngay_thieu_gio'] * 100000) + $row['bonus'];
+            ?>
+            <tr>
+                <td><?php echo $row['fisrt_name'] . " " . $row['last_name']; ?></td>
+                <td><?php echo number_format($row['salary']); ?></td>
+                <td><?php echo number_format($row['bonus']); ?></td>
+                <td><?php echo $row['tong_so_ngay_cong']. "/". $cong_thuc_te; ?></td>
+                <td><?php echo $row['so_ngay_thieu_gio']; ?></td>
+                <td><?php echo number_format($total_salary); ?></td>
+            </tr>
+            <?php
+                }
+            ?>
+        </table>
+    </div>
+        
 </body>
 <script src="./js/index.js"></script>
 </html>
---
+++ templates/payroll_details.php
@@ -0,0 +1,292 @@
+<?php
+    require 'connect_database.php';
+    mysqli_set_charset($connect, 'UTF8');
+    $id = $_GET['id'];
+    $sql = "SELECT count(*) as tong_so_ngay_cong, COUNT(CASE WHEN a.total < '08:00:00' THEN 1 ELSE NULL END) as so_ngay_thieu_gio, u.*, s.* FROM `user_data` u INNER JOIN `salary_and_bonus` s ON u.id = s.employee_id INNER JOIN `attendance` a ON u.user_id = a.user_id WHERE a.total > '00:00:00' AND u.id = $id GROUP BY a.user_id;";
+    $result = $connect->query($sql);
+    $employee = $result->fetch_assoc();
+    $user_id = $employee['user_id'];
+    $sql = "SELECT * FROM `attendance` WHERE user_id = '$user_id';";
+    $result = $connect->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8 vi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>HRM</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <link href="./icons/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" href="./image/icon-image.png">
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <!-- Js -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
+    <style>
+        .link_home {
+            margin-right: 10px;
+            background-color: #9FD7F9;
+            color: black;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            margin: 20px 10px;
+        }
+        .blue-box {
+            padding: 10px; /* Khoảng cách giữa nội dung và viền của ô */
+            border-radius: 5px; /* Bo tròn viền của ô */
+            text-align: center; /* Canh giữa nội dung */
+            margin: 20px 10px;
+        }
+        .blue-box h1 {
+            color: black; /* Màu chữ trắng */
+            margin: 0; /* Xóa khoảng cách lề */
+        }
+        #information-table {
+            border-collapse: collapse;
+            width: 100%;
+            height: auto;
+        }
+        #information-table th, 
+        #information-table td {
+            border: 1px solid #dddddd;
+            padding: 8px;
+            text-align: left;
+        }
+        #information-table tr:first-child th {
+            position: sticky; /* Giữ vị trí */
+            top: 0; /* Vị trí cố định ở trên */
+            background-color: #9FD7F9; /* Màu nền của hàng đầu tiên */
+        }
+    </style>
+</head>
+<body>
+    <header class="header">
+        <div class="hrm-title">
+            <div class="title close">
+                <h4>HRM Admin</h4>
+            </div>
+            <div class="icon-nav close">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        <!--header account-->
+        <div class="account" id="dropdown">
+            <div class="image-account">
+                <img class="avatar-account" src="./image/icon-image.png" alt="hrm-icon">
+            </div>
+
+            <?php
+                if (isset($_SESSION['nameaccount']))
+                {
+                    echo "<div class='account-title'>
+                        <h4>".$_SESSION['nameaccount']."</h4>
+                    </div>";
+                }
+            ?>
+
+
+            <div class="icon-account">
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+            
+            <div class="dropdown-content">
+                <a href="attendance.php">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    Chấm Công
+                </a>
+                <a href="javascript:void(0);" onclick="confirmLogout()">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    Đăng xuất
+                </a>
+                
+
+            </div>
+            <script>
+            function confirmLogout() {
+                if (confirm("Bạn có chắc chắn muốn đăng xuất?")) {
+                window.location.href = "logout.php";
+                    }
+                }
+            </script>
+        </div>
+
+    </header>
+
+    <div class="grid_system_column close container">
+        <!-- 20% -->
+        <div class="container-nav_bar">
+            <div class="nav_bar-function">
+                 <div class="nav_bar-function-content close"> <!-- Quản lí nhân viên -->
+                    <i class="nav_bar-function-icon fa-solid fa-sitemap fa-lg"></i>
+                    <a>Quản lí nhân viên</a>
+                    <div class="function-icon_arrow_Manager">
+                        <i class="nav_bar-function-icon fa-solid fa-angle-up"></i>
+                    </div>
+                </div>
+                <div class="nav_bar-function_child">
+                    <ul class="nav_bar-function_child_Manager none">
+                        <li class="nav_bar-list-item">
+                            <a href="employee-information.php">Quản lý nhân viên</a>
+                        </li>
+                        <li class="nav_bar-list-item"><a href="salary.php">Bảng lương</a></li>
+                        <li class="nav_bar-list-item"><a href="benefit.php">Bảo hiểm, đãi ngộ</a></li>
+                        <li class="nav_bar-list-item"><a href="performance.php">Hiệu suất</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="nav_bar-function">
+                <div class="nav_bar-function-content close">
+                    <i class="nav_bar-function-icon fa-solid fa-calendar-days fa-lg"></i>
+                    <a>Báo cáo chấm công</a>
+                    <div class="function-icon_arrow_Report">
+                        <i class="nav_bar-function-icon fa-solid fa-angle-up"></i>
+                    </div>
+                </div>
+                <div class="nav_bar-function_child">
+                    <ul class="nav_bar-function_child_Report none">
+                        <li class="nav_bar-list-item"><a href="attendance_report.php">Danh sách chấm công</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <div class="nav_bar-function">
+                <div class="nav_bar-function-content close">
+                    <i class="nav_bar-function-icon fa-solid fa-envelopes-bulk fa-lg"></i>
+                    <a>Đơn & giải trình</a>
+                    <div class="function-icon_arrow_Assignment">
+                        <i class="nav_bar-function-icon fa-solid fa-angle-up"></i>
+                    </div>
+                </div>
+                <div class="nav_bar-function_child">
+                    <ul class="nav_bar-function_child_Assignment none">
+                        <?php
+                            if(isset($_SESSION['role'])){
+                                if($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'manager'){
+                                    echo '<li class="nav_bar-list-item"><a href="approve_form.php">Duyệt đơn</a></li>
+                                    <li class="nav_bar-list-item"><a href="unexcused.php">Nghỉ không phép</a></li>';
+                                }
+                            }
+                        ?>
+
+                        <?php
+                            if(isset($_SESSION['role'])){
+                                if($_SESSION['role'] == 'employee'){
+                                    echo '<li class="nav_bar-list-item"><a href="form_employee.php">Gửi đơn</a> </li>';
+                                }
+                            }
+                        ?>
+                        
+                    </ul>
+                </div>
+                
+                <?php
+                    if(isset($_SESSION['role'])){
+                        if($_SESSION['role'] == 'admin' ){
+                            echo '<div class="nav_bar-function">
+                            <div class="nav_bar-function-content close">
+                                <i class="nav_bar-function-icon fa-solid fa-code fa-lg"></i>
+                                <a>Admin Console</a>
+                                <div class="function-icon_arrow_AdminConsole">
+                                    <i class="nav_bar-function-icon fa-solid fa-angle-up"></i>
+                                </div>
+                            </div>
+                            <div class="nav_bar-function_child">
+                                <ul class="nav_bar-function_child_AdminConsole none">
+                                    <a class="nav_bar-list-item" href="create-accounts.php">Hiệu suất</a>
+                                </ul>
+                            </div>
+                        </div>';
+                        }
+                        if($_SESSION['role'] == 'manager' ){
+                            echo '<div class="nav_bar-function">
+                            <div class="nav_bar-function-content close">
+                                <i class="nav_bar-function-icon fa-solid fa-code fa-lg"></i>
+                                <a>Manager Console</a>
+                                <div class="function-icon_arrow_AdminConsole">
+                                    <i class="nav_bar-function-icon fa-solid fa-angle-up"></i>
+                                </div>
+                            </div>
+                            <div class="nav_bar-function_child">
+                                <ul class="nav_bar-function_child_AdminConsole none">
+                                    <a class="nav_bar-list-item" href="performance_detail.php">Bàn giao công việc</a>
+                                </ul>
+                            </div>
+                        </div>';
+                        }
+                    }
+                    
+                ?>
+
+
+            </div>
+        </div>
+    <div class="form-edit">
+        <a class="link_home" href='employee-information.php'>Trang chủ</a>
+        <div class="blue-box">
+            <h1>Thông tin nhân viên chi tiết</h1>
+        </div>
+        <div class="form-data_manager-table">
+            <table id="information-table">
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Position</th>
+                    <th>Department</th>
+                    <th>Salary</th>
+                    <th>Bonus</th>
+                    <th>Total Working Days</th>
+                    <th>Short working days</th>
+                </tr>
+                <tr>
+                    <td><?php echo $employee['fisrt_name']; ?></td>
+                    <td><?php echo $employee['last_name']; ?></td>
+                    <td><?php echo $employee['email']; ?></td>
+                    <td><?php echo $employee['phone']; ?></td>
+                    <td><?php echo $employee['address']; ?></td>
+                    <td><?php echo $employee['position']; ?></td>
+                    <td><?php echo $employee['department']; ?></td>
+                    <td><?php echo $employee['salary']; ?></td>
+                    <td><?php echo $employee['bonus']; ?></td>
+                    <td><?php echo $employee['tong_so_ngay_cong']; ?></td>
+                    <td><?php echo $employee['so_ngay_thieu_gio']; ?></td>
+                </tr>
+            </table>
+
+            <table id="information-table">
+                <tr>
+                    <th>STT</th>
+                    <th>Ngày</th>
+                    <th>Checkin</th>
+                    <th>Checkout</th>
+                    <th>Total</th>
+                </tr>
+                <?php
+                    $count = 0;
+                    while ($row = $result->fetch_assoc()) {
+                        $count++;
+                ?>
+                <tr>
+                    <td><?php echo $count; ?></td>
+                    <td><?php echo $row['date']; ?></td>
+                    <td><?php echo $row['check_in']; ?></td>
+                    <td><?php echo $row['check_out']; ?></td>
+                    <td><?php echo $row['total']; ?></td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
+</body>
